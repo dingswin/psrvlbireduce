be3e8e6abd334d5c0fb6abd485008714aa2c8017
Hao Ding <ding.hao@hotmail.com>
2019-03-15 10:55:52 +1100

    pass inbeamsn to phscal

--- datareduction/final_astrometric_reduce.py
+++ datareduction/final_astrometric_reduce.py
@@ -22,7 +22,6 @@ from time import gmtime, strftime
 from optparse import OptionParser
 warnings.defaultaction = "always"
 
-#Hao tries
 
 ################################################################################
 # Little logger class to put print statements to the log file
@@ -1772,7 +1771,7 @@ if runfromlevel <= runlevel and runtolevel >= runlevel and \
         if not os.path.exists(calibtablepath):
             haveall = False
     if not targetonly and (not haveall or \
-           not interaction.yesno("Do you wish to used saved SN table for phsref phase CALIB?")):
+           not interaction.yesno("Do you wish to use saved SN table for phsref phase CALIB?")):
         print "Runlevel " + str(runlevel) + ": Running CALIB on phsref sources"
         for phscal, config in zip(donephscalnames, doneconfigs):
             for i in range(20): #Clear any old CALIB split catalog entries
@@ -2787,6 +2786,77 @@ else:
 
 runlevel  = runlevel + 1
 printTableAndRunlevel(runlevel, snversion, clversion+targetcl, inbeamuvdatas[0])
+## Pass inbeamsn back to phscal ################################################
+expconfig     = yaml.load(open(expconfigfile))
+expdir        = expconfig['rootdir'] + '/' + experiment + '/'
+numtargets    = len(expconfig['targets'])
+targetconfigs = []
+for i in range(numtargets):
+    targetconfigfile = configdir + expconfig["targets"][i] + '.yaml'
+    if not os.path.exists(targetconfigfile):
+        parser.error("Target config file %s does not exist!" % targetconfigfile)
+    targetconfigs.append(yaml.load(open(targetconfigfile)))
+
+for i in range(numtargets):
+    targetname = expconfig["targets"][i]
+    targetconfig = targetconfigs[i]
+    sourcefile = expdir + experiment + '.source'
+    sourcefilelines = open(sourcefile).readlines()
+    phsrefname = ""
+    modeltypestr = 'final'
+    for j in range(len(sourcefilelines)):
+        if targetname in sourcefilelines[j]:
+            phsrefname = sourcefilelines[j+1].split(':')[-1].strip()
+    if phsrefname == "":
+        print "Couldn't find the phase reference source for " + targetname
+        sys.exit()
+    if expconfig['useprelimmodels']:
+        modeltypestr = 'preliminary'
+    aips8phsrefname = phsrefname
+    aips12phsrefname = phsrefname
+    if len(phsrefname) > 8:
+        aips8phsrefname = phsrefname[:8]
+    if len(phsrefname) > 12:
+        aips12phsrefname = phsrefname[:12]
+    phsrefdatafile = expdir + experiment + '_' + phsrefname + '_pipeline_uv.fits'
+    phsrefimagefile = auxdir + 'sourcemodels/' + modeltypestr + '/' + phsrefname + '.clean.fits'
+    shiftedfile = expdir + experiment + '_' + phsrefname + '_ibshiftdiv_uv.fits'
+    jmfitfile = expdir + experiment + '_' + phsrefname + '_ibshiftdiv.jmfit'
+    phsrefdata = AIPSUVData(aips8phsrefname, "TEMP", 1, 1)
+    phsrefimage = AIPSImage(aips12phsrefname, "TMPCLN", 1, 1)
+    splatdata = AIPSUVData(aips8phsrefname, "SPLAT", 1, 1)
+    normdata = AIPSUVData(aips8phsrefname, "NORM", 1, 1)
+    shiftedimage = AIPSImage(aips12phsrefname, "ICL001", 1, 1)
+    shiftedbeam = AIPSImage(aips12phsrefname, "IBM001", 1, 1)
+    if phsrefdata.exists():
+        phsrefdata.zap()
+    if splatdata.exists():
+        splatdata.zap()
+    if phsrefimage.exists():
+        phsrefimage.zap()
+    if normdata.exists():
+        normdata.zap()
+    if shiftedimage.exists():
+        shiftedimage.zap()
+    if shiftedbeam.exists():
+        shiftedbeam.zap()
+    vlbatasks.fitld_uvfits(phsrefdatafile, phsrefdata, "")
+    vlbatasks.fitld_image(phsrefimagefile, phsrefimage)
+    if "," in targetconfig['primaryinbeam']:
+        caltable = expdir + 'tables/CONCAT0.icalib.p1.sn'
+    else:
+        caltable = expdir + 'tables/' + targetconfig['primaryinbeam'] + '.icalib.p1.sn'
+    if not os.path.exists(caltable):
+        print caltable + ' does not exist'
+        sys.exit()
+    vlbatasks.loadtable(phsrefdata, caltable, 1)
+    vlbatasks.splat(phsrefdata, 1, [0], splatdata, 1, 0)
+    vlbatasks.normaliseUVData(splatdata, phsrefimage, normdata)
+    vlbatasks.writedata(normdata, shiftedfile, True)
+    vlbatasks.widefieldimage(normdata, aips12phsrefname, 256, 0.75, True, 0.050,
+                   0, 0, 0, 100, 20)
+    vlbatasks.nonpulsarjmfit("", jmfitfile, aips12phsrefname, -1, -1, True,
+                             False,shiftedimage,48)
 ## Make some nice diagnostic plots #############################################
 if runfromlevel <= runlevel and runtolevel >= runlevel:
     print "Making final diagnostic plots"
@@ -2796,4 +2866,3 @@ if runfromlevel <= runlevel and runtolevel >= runlevel:
     os.chdir(cwd)
 else:
     print "Skipping making of diagnostic plots"
-
