cae13c444bf3478ae25fc1b40481f68cf66f32e1
Hao Ding <ding.hao@hotmail.com>
2019-05-03 19:51:11 +1000

    fix the bug that appears when the observation is inter-daily (e.g. 23:30 to 0:30)

--- datareduction/prepare_astrometric_epoch.py
+++ datareduction/prepare_astrometric_epoch.py
@@ -6,14 +6,32 @@ usage = "prepare_astrometric_epoch.py <.vex file>"
 if len(sys.argv) != 2:
     print usage
     sys.exit()
+vexfile = sys.argv[1]
+experiment = vexfile.split('.')[0].strip()
+VLBAkeyfileftproot = 'www.vlba.nrao.edu/astro/VOBS/astronomy/'
+sumfile = experiment + '.sum'
 
-vexin = open(sys.argv[1])
+if not os.path.exists(vexfile):
+    print("\n%s doesn't exists. trying to download one...\n" % vexfile)
+    obsmonth = raw_input("What's the observation month (in mmmyy format, e.g. mar19)\n")
+    VLBAkeyfileftpdir = VLBAkeyfileftproot + obsmonth + '/' + experiment + '/'
+    os.system('wget -T 5 %s%s' % (VLBAkeyfileftpdir, vexfile))
+if not os.path.exists(vexfile):
+    print("%s not found on ftp server; aborting\n" % vexfile)
+    sys.exit()
+
+vexin = open(vexfile)
 vexlines = vexin.readlines()
 vexin.close()
 
 startfound = False
 stopfound = False
 for line in vexlines:
+    if 'date' in line:
+        obsdate  = line.split(':')[-1].split(' ')
+        obsmonth = obsdate[-2].strip().lower()
+        obsmonth = obsmonth + obsdate[-1].strip()[2:4]
+        print obsmonth
     if 'MJD' in line:
         MJD = int(line.split(':')[-1])
     if "exper_nominal_start" in line:
@@ -35,10 +53,20 @@ if not (startfound and stopfound):
     print "Couldn't find start and/or stop date! Aborting."
     sys.exit()
 
+if not os.path.exists(sumfile):
+    print("try to download %s.sum...\n" % experiment)
+    VLBAkeyfileftpdir = VLBAkeyfileftproot + obsmonth + '/' + experiment + '/'
+    os.system('wget -T 5 %s%s.sum' % (VLBAkeyfileftpdir, experiment))
+
 os.system("mkdir logs")
 os.system("mkdir tables")
 os.system("mkdir images")
 os.chdir("logs")
+print "\ndeleting logfiles...\n"
+try:
+    os.system("rm *")
+except OSError:
+    pass
 os.system("wget -T 12 https://vlbi.gsfc.nasa.gov/apriori/usno_finals.erp")
 # in case the erp file service was down...
 if not os.path.exists("usno_finals.erp"):
@@ -51,13 +79,6 @@ if not os.path.exists("usno_finals.erp"):
     else:
         os.system("wget ftp://ftp.lbo.us/pub/staff/wbrisken/EOP/usno_finals.erp")
 
-if edoy != sdoy:
-    sdoy  = edoy
-    syear = eyear
-try:
-    os.system("rm *.Z")
-except OSError:
-    pass
 os.system("wget ftp://cddis.gsfc.nasa.gov/gps/products/ionex/%04d/%03d/*.Z" % (syear, sdoy))
 if os.path.exists("igsg%03d0.%02di.Z" % (sdoy,syy)):
     print ("\ngunzip igsg%03d0.%02di.Z...\n" % (sdoy,syy))
@@ -65,3 +86,13 @@ if os.path.exists("igsg%03d0.%02di.Z" % (sdoy,syy)):
 else:
     print ("\ngunzip jgsg%03d0.%02di.Z...\n" % (sdoy,syy))
     os.system("gunzip jplg%03d0.%02di.Z" % (sdoy, syy))
+if edoy != sdoy:
+    os.system("wget ftp://cddis.gsfc.nasa.gov/gps/products/ionex/%04d/%03d/*.Z" % (eyear, edoy))
+    if os.path.exists("igsg%03d0.%02di" % (sdoy,syy)) and os.path.exists("igsg%03d0.%02di.Z" % (edoy,eyy)):
+        print ("\ngunzip igsg%03d0.%02di.Z...\n" % (edoy,eyy))
+        os.system("gunzip igsg%03d0.%02di.Z" % (edoy,eyy))
+    else:
+        print ("\ngunzip jgsg%03d0.%02di.Z...\n" % (edoy,eyy))
+        os.system("gunzip jplg%03d0.%02di.Z" % (edoy, eyy))
+        
+
