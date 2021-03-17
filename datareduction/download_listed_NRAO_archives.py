06bff8abdd4a2daa3aeec23d3cba75d02d1fd362
Hao Ding <ding.hao@hotmail.com>
2021-03-17 18:52:52 +1100

    download_listed*.py has been upgraded to download public VLBA data

--- datareduction/download_listed_NRAO_archives.py
+++ datareduction/download_listed_NRAO_archives.py
@@ -4,25 +4,32 @@ import os, sys
 copy the archive info to a downloads.txt at the webpage step to select 'Retrieve over internet',
 then you have to copy and paste webpage link into the downloads.txt after a '#' symbol, and copy user and password in the same line separated with '#',
 then run download*py downloads.txt
-An example can be found at /fred/oz002/hding/mspsrpi/processing/J1818-1607/bd232b/downloads.txt
+An example can be found at /fred/oz002/hding/mspsrpi/processing/J1818-1607/bd232b/downloads.txt for locked data;
+another example at /fred/oz002/hding/mspsrpi/processing/J1853+1303/downloads.txt.
 """
 usage = "download_listed_NRAO_archives.py downloads.txt\n"
 if len(sys.argv) != 2:
     print usage
     sys.exit()
 [junk, downloadlistfile] = sys.argv
+public_data = True
 lines = open(downloadlistfile).readlines()
 for line in lines:
     if line.startswith('#'):
         line = line.split('#')
         link = line[1].strip()
-        user = line[2].strip()
-        passwd = line[3].strip()
+        if len(line) == 4:
+            public_data = False
+            user = line[2].strip()
+            passwd = line[3].strip()
     #prompt = 'Public File available : '
     #if prompt in line:
     else:
         line = line.replace('  ', ' ').replace('  ', ' ')
         fitsfile = line.strip().split(' ')[0].strip()
         fitsfile = link + '/' + fitsfile
-        os.system('wget -t 0 --user %s --password %s %s' % (user, passwd, fitsfile))
+        if public_data:
+            os.system('wget -t 0 %s' % fitsfile)
+        else:
+            os.system('wget -t 0 --user %s --password %s %s' % (user, passwd, fitsfile))
 print "Downloads complete"
